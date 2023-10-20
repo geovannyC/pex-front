18660f0f0eaae919b4e483f4cf8044c0214c8948
geovannyC <srmaeson@outlook.com>
2023-10-20 17:51:20 -0500

    builded

---
+++ .next/static/chunks/app/pages/principal/page-1e1a99d916b269e8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[418],{5199:function(n,e,u){Promise.resolve().then(u.bind(u,8107))}},function(n){n.O(0,[463,780,971,596,744],function(){return n(n.s=5199)}),_N_E=n.O()}]);
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import Principal from "./pages/principal/page";
-import React from "react";
-import "../../styles/globals.css";
-export default function Home() {
-  return <Principal />;
-}
